2766db0d143d1f75690069f80cba31ca133aa931
T.I.OAK <tioak.puz@gmail.com>
2026-04-27 21:20:14 +0900

    feat: Implement Item constructor and base properties

---
+++ src/systems/entities/Item.js
@@ -0,0 +1,54 @@
+import DataManager from '../../core/DataManager.js';
+
+/**
+ * Item クラス
+ * ロケット構成パーツおよび発射装備の基体となるエンティティ
+ */
+class Item {
+    #master;
+
+    constructor(masterId) {
+        this.#master = DataManager.getItemById(masterId);
+        
+        // 識別子生成
+        this.uid = 'item_' + Date.now() + '_' + Math.random().toString(36).substr(2, 9);
+        
+        this.id = this.#master.id;
+        
+        // 強化関連初期化
+        this.enhancement = {};
+        this.enhancementCount = 0;
+
+        // 表示・基本情報
+        this.name = this.#master.name || 'Unknown';
+        this.category = this.#master.category || 'misc';
+        this.rarity = this.#master.rarity || 'common';
+        this.description = this.#master.description || '';
+        
+        // 加算系プロパティ (デフォルト 0)
+        this.mass = this.#master.mass ?? 0;
+        this.slots = this.#master.slots ?? 0;
+        this.precision = this.#master.precision ?? 0;
+        this.pickupRange = this.#master.pickupRange ?? 0;
+        this.power = this.#master.power ?? 0;
+        this.maxCharges = this.#master.maxCharges ?? 0;
+        this.duration = this.#master.duration ?? 0;
+        
+        // 乗算系プロパティ (デフォルト 1.0)
+        this.precisionMultiplier = this.#master.precisionMultiplier ?? 1.0;
+        this.pickupMultiplier = this.#master.pickupMultiplier ?? 1.0;
+        this.gravityMultiplier = this.#master.gravityMultiplier ?? 1.0;
+        this.powerMultiplier = this.#master.powerMultiplier ?? 1.0;
+        this.arcMultiplier = this.#master.arcMultiplier ?? 1.0;
+        
+        // フラグ・特殊 (デフォルト false / 文字列)
+        this.onLostBonus = this.#master.onLostBonus ?? false;
+        this.ghostType = this.#master.ghostType; // 仕様では特にデフォルト指定なし。undefinedならそのまま
+        this.preventsLauncherWear = this.#master.preventsLauncherWear ?? false;
+
+        // 初期状態の耐久度
+        this.charges = this.maxCharges;
+    }
+}
+
+export default Item;
